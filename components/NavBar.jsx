eeafcdd71790aa6640d831725f6738ff084a5b80
Rashad <rashadabayram@gmail.com>
2022-06-01 23:25:12 -0600

    added responsive hamburger navmenu

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import MenuIcon from "../assets/navmenu/menuicon.png";
+import CloseIcon from "../assets/navmenu/x.png";
 
 const NavBar = () => {
-  const navmenu = ["Home", "Posts", "Cool Stuff", "About Me"];
-  const tools = ["mds", "rss"];
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState();
+
+  const navmenu = ["HOME", "POSTS", "STUFF", "ABOUT"];
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
   return (
     <header className="header">
       <span className="logo">Rashad Bayram</span>
-
-      <nav className="menu">
-        {navmenu.map((menuitem, key) => (
-          <span key={key}>
-            <a className="subitems" href="#">
-              {menuitem}
-            </a>
-          </span>
-        ))}
-      </nav>
-      <span className="tools">
-        {tools.map((tool, key) => (
-          <span className="tool" key={key}>
-            <a className="subitems" href="#">
-              {tool}
-            </a>
-          </span>
-        ))}
+      {(toggleMenu || screenWidth > 768) && (
+        <nav className="menu">
+          {navmenu.map((menuitem, key) => (
+            <span key={key}>
+              <a className="subitems" href="#">
+                {menuitem}
+              </a>
+            </span>
+          ))}
+        </nav>
+      )}
+      <span onClick={toggleNav} className="btn menuicon">
+        <a href="#">
+          {!toggleMenu ? (
+            <Image src={MenuIcon} alt="menuicon" width={50} height={50} />
+          ) : (
+            <Image src={CloseIcon} alt="menuicon" width={50} height={50} />
+          )}
+        </a>
       </span>
     </header>
   );
