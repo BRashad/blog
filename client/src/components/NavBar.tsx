73f4ff5a72875ce6a1fcece039fb1d7b7e600f83
Rashad <rashadabayram@gmail.com>
2022-07-06 08:22:39 -0600

    minor refactor in styles

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -69,25 +69,6 @@ export default function NavBar() {
           </li>
         </ul>
       </nav>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
     </div>
   );
 }
