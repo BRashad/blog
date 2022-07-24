4c449d2c7d7852acafc570c1fd0c71f118085b83
Rashad <rashadabayram@gmail.com>
2022-07-24 03:38:19 -0600

    nodemon setup and server.js reconfig

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
 });
