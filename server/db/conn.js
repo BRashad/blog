a1667c08279d27f0709d700391ba1e0f44b23418
Rashad <rashadabayram@gmail.com>
2022-07-24 03:06:33 -0600

    redo the back end

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,25 +1,25 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const { MongoClient } = require("mongodb");
+// const Db = process.env.ATLAS_URI;
+// const client = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-console.log("DataBase", Db);
+// console.log("DataBase", Db);
 
-var _db;
+// var _db;
 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (db) {
-        _db = db.db("posts");
-        console.log("Successfully connected to MongoDB!");
-      }
-      return callback(err);
-    });
-  },
-  getDb: function () {
-    return _db;
-  },
-};
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       if (db) {
+//         _db = db.db("posts");
+//         console.log("Successfully connected to MongoDB!");
+//       }
+//       return callback(err);
+//     });
+//   },
+//   getDb: function () {
+//     return _db;
+//   },
+// };
--- server/server.js
+++ server/server.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-
+const PORT = 4000;
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/post"));
-
-const dbo = require("./db/conn");
-
-app.listen(port, () => {
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
 });
